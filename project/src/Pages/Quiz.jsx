089a917d9f40c9e4c11fb411b8105d360b6720da
ilyassbnh <benhida.ilyass@gmail.com>
2025-11-13 12:30:56 +0100

    quiz.jsx , result.jsx

--- project/src/Pages/Quiz.jsx
+++ project/src/Pages/Quiz.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { use } from "react";
 import questions from "../data/Questions";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function QuizPage() {
-        const [count , setCount ] = useState(0)
-        const nextOption = count +1
 
-        if( nextOption < options.length){
-            setCount(nextOption)
+    const [count, setCount] = useState(0)
+    const [score, setScore] = useState(0)
+    const navigate = useNavigate()
+
+
+    //gestion des click des opitons
+    const answerClick = (value) => {
+
+        //verification des choix
+        if (count < questions.length - 1) {
+            if (value === questions[count].answer) {
+                setScore(score => score + 1)
+            }
+            //next question count
+            setCount(count => count + 1)
+
+        } else {
+            navigate('/ResultPage',{state:{ score: score +(value===questions[count].answer ? 1 : 0),total:questions.length}})
         }
+
+    }
 
 
     return (
         <div className="content">
-            <div className="question">{questions[0].question}</div>
+            <div className="question">{questions[count].question}</div>
             <div className="choiceGroup">
-                {questions[1].options.map((option,i) => (
-                    <button key={i}> {option} </button>
+                {questions[count].options.map((option, i) => (
+                    <button key={i} onClick={() => answerClick(option)}> {option} </button>
                 ))}
             </div>
         </div>
--- project/src/Pages/Result.jsx
+++ project/src/Pages/Result.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-
+import { useLocation, useNavigate } from "react-router-dom";
 function ResultPage() {
+    const  Nav = useNavigate()
+    const local = useLocation()
+    const score = local.state?.score || 0 
+    const total = local.state?.total || 10
   return (
     <div className="page-content">
       <h1>Your Results</h1>
-      <p>You scored 0 out of 1!</p>
+      <p>You scored {score} out of {total}</p>
+      <button className="start-button" onClick={()=>Nav('/')}>Back Home</button>
     </div>
   );
 }
