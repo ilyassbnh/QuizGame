8c060760824689ca3e19c703d6eedadb134e55a2
ilyassbnh <benhida.ilyass@gmail.com>
2025-11-14 09:48:39 +0100

    message de resultat

--- project/src/Pages/Result.jsx
+++ project/src/Pages/Result.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 function ResultPage() {
-    const  Nav = useNavigate()
-    const local = useLocation()
-    const score = local.state?.score || 0 
-    const total = local.state?.total || 10
+  const Nav = useNavigate()
+  const local = useLocation()
+  const score = local.state?.score || 0
+  const total = local.state?.total || 10
+  let message;
+
+  if (score < 5) {
+    message = "Better luck next time"
+  } else if (score <= 7) {
+    message = "Good"
+  } else {
+    message = "Exellent"
+  }
+
+
+
   return (
     <div className="page-content">
-      <h1>Your Results</h1>
+      <h1>{message}</h1>
+
       <p>You scored {score} out of {total}</p>
-      <button className="start-button" onClick={()=>Nav('/')}>Back Home</button>
+      <button className="start-button" onClick={() => Nav('/')}>Back Home</button>
     </div>
   );
 }
